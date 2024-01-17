f5d61c60f7c04a6b51939746749c65f94c8a1d59
Ilya Safonov <safonovil17@yandex.ru>
2024-01-17 13:37:18 +0300

    base markup

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./index.module.scss";
 
 export const App: React.FC = () => {
-    return (
-        <article>123123</article>
-    )
-}
+  return (
+    <article className={styles.article}>
+      <h1 className={styles.articleTitle}>Список задач</h1>
+      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}></section>
+    </article>
+  );
+};

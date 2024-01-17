a90d5bbd790b9b69f394a81d1a298ac46b5378fb
Ilya Safonov <safonovil17@yandex.ru>
2024-01-17 13:45:32 +0300

    described the interface for todostore

--- src/data/stores/useToDoStore.ts
+++ src/data/stores/useToDoStore.ts
@@ -1,7 +1,14 @@
-import create from 'zustand'
+import create from "zustand";
 
 interface Task {
-    id: string,
-    title: string,
-    createdAt: number
+  id: string;
+  title: string;
+  createdAt: number;
+}
+
+interface ToDoStore {
+  tasks: Task[];
+  createTask: (title: string) => void;
+  updatedTask: (id: string, title: string) => void;
+  removeTask: (id: string) => void;
 }

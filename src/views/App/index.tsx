dccdf35b3c29f6e0c3142e30f8c442e198eed70e
Ilya Safonov <safonovil17@yandex.ru>
2024-01-18 12:52:20 +0300

    added markup for the task section

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Input } from '../components/Input'
 import { useToDoStore } from '../../data/stores/useToDoStore'
 import styles from './index.module.scss'
+import { Task } from '../components/Task'
 
 export const App: React.FC = () => {
 	const [tasks, createTask, updateTask, removeTask] = useToDoStore(state => [
@@ -21,7 +22,21 @@ export const App: React.FC = () => {
 					}}
 				/>
 			</section>
-			<section className={styles.articleSection}></section>
+			<section className={styles.articleTasks}>
+				{!tasks.length && (
+					<p className={styles.articleTasksStub}>Нет ни одной задачи</p>
+				)}
+				{tasks.map(task => (
+					<Task
+						key={task.id}
+						id={task.id}
+						title={task.title}
+						onDone={removeTask}
+						onEdite={updateTask}
+						onRemove={removeTask}
+					/>
+				))}
+			</section>
 		</article>
 	)
 }

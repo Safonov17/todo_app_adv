12e6c590023ac35b3ea4c48c9a5b9a0da10e2f1b
Ilya Safonov <safonovil17@yandex.ru>
2024-01-18 14:53:17 +0300

    Added logic to the Task component

--- src/views/components/Task/index.tsx
+++ src/views/components/Task/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import styles from './index.module.scss'
 
 interface TaskProps {
@@ -17,12 +17,20 @@ export const Task: React.FC<TaskProps> = ({
 	title
 }) => {
 	const [checked, setChecked] = useState(false)
+	const [isEditMode, setIsEditMode] = useState(false)
+	const [value, setValue] = useState(title)
+	const editTitleInputRef = useRef<HTMLInputElement>(null)
+
+	useEffect(() => {
+		if (isEditMode) editTitleInputRef?.current?.focus()
+	}, [isEditMode])
 
 	return (
 		<div className={styles.task}>
 			<label className={styles.taskLabel}>
 				<input
 					type="checkbox"
+					disabled={isEditMode}
 					checked={checked}
 					className={styles.taskCheckbox}
 					onChange={evt => {
@@ -30,20 +38,49 @@ export const Task: React.FC<TaskProps> = ({
 						if (evt.target.checked) onDone(id)
 					}}
 				/>
-				<h3 className={styles.taskTitle}>{title}</h3>
+				{isEditMode ? (
+					<input
+						className={styles.taskTitleEdit}
+						value={value}
+						ref={editTitleInputRef}
+						onChange={evt => setValue(evt.target.value)}
+						onKeyDown={evt => {
+							if (evt.key === 'Enter') {
+								onEdite(id, value)
+								setIsEditMode(false)
+							}
+						}}
+					/>
+				) : (
+					<h3 className={styles.taskTitle}>{title}</h3>
+				)}
+			</label>
+
+			{isEditMode ? (
 				<button
-					aria-label="Edit"
-					className={styles.taslEdit}
-					onClick={() => {}}
-				/>
-				<button
-					aria-label="Remove"
-					className={styles.taslRemove}
+					aria-label="Sace"
+					className={styles.taskSave}
 					onClick={() => {
-						if (confirm('Вы уверены?')) onRemove(id)
+						onEdite(id, value)
+						setIsEditMode(false)
 					}}
 				/>
-			</label>
+			) : (
+				<button
+					aria-label="Edit"
+					className={styles.taskEdit}
+					onClick={() => {
+						setIsEditMode(true)
+					}}
+				/>
+			)}
+			<button
+				aria-label="Remove"
+				className={styles.taskRemove}
+				onClick={() => {
+					if (confirm('Вы уверены?')) onRemove(id)
+				}}
+			/>
 		</div>
 	)
 }

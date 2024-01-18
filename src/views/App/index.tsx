0a758d3d86cf803b3e346c45e1d795cee02b1799
Ilya Safonov <safonovil17@yandex.ru>
2024-01-18 09:32:43 +0300

    Create 'app' component

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
-import styles from "./index.module.scss";
+import React from 'react'
+import { Input } from '../components/Input'
+import { useToDoStore } from '../../data/stores/useToDoStore'
+import styles from './index.module.scss'
 
 export const App: React.FC = () => {
-  return (
-    <article className={styles.article}>
-      <h1 className={styles.articleTitle}>Список задач</h1>
-      <section className={styles.articleSection}></section>
-      <section className={styles.articleSection}></section>
-    </article>
-  );
-};
+	const [tasks, createTask, updateTask, removeTask] = useToDoStore(state => [
+		state.tasks,
+		state.createTask,
+		state.updateTask,
+		state.removeTask
+	])
+
+	return (
+		<article className={styles.article}>
+			<h1 className={styles.articleTitle}>Список задач</h1>
+			<section className={styles.articleSection}>
+				<Input
+					onAdd={title => {
+						if (title) createTask(title)
+					}}
+				/>
+			</section>
+			<section className={styles.articleSection}></section>
+		</article>
+	)
+}
